fed81e10a73d5c7fc6330a28b32f19de9f5123a2
AlekseyZosyak <110170863+AlekseyZosyak@users.noreply.github.com>
2026-06-03 15:06:52 +0100

    Update navigation and homepage styles

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -11,20 +11,22 @@ const Nav = () => {
                 <span className="nav__brand-dot"></span>
                 <span>Green Screen</span>
             </a>
-            <ul className='nav__list'>
-                <li className='nav__list-item'>
-                    <NavLink className={getLinkClass} to={'/'}>Home</NavLink>
-                </li>
-                <li className='nav__list-item'>
-                    <NavLink className={getLinkClass} to={'/about'}>About</NavLink>
-                </li>
-                <li className='nav__list-item'>
-                    <NavLink className={getLinkClass} to={'/setting'}>Setting</NavLink>
-                </li>
-            </ul>
-            <a className="nav__github-link" href="https://github.com/alexzosiak/green-screen" aria-label="GitHub">
-                <img src={github} alt="" className='nav__github'/>
-            </a>
+            <div className="nav__actions">
+                <ul className='nav__list'>
+                    <li className='nav__list-item'>
+                        <NavLink className={getLinkClass} to={'/'}>Home</NavLink>
+                    </li>
+                    <li className='nav__list-item'>
+                        <NavLink className={getLinkClass} to={'/about'}>About</NavLink>
+                    </li>
+                    <li className='nav__list-item'>
+                        <NavLink className={getLinkClass} to={'/setting'}>Setting</NavLink>
+                    </li>
+                </ul>
+                <a className="nav__github-link" href="https://github.com/alexzosiak/green-screen" aria-label="GitHub">
+                    <img src={github} alt="" className='nav__github'/>
+                </a>
+            </div>
         </div>
     )
 }
